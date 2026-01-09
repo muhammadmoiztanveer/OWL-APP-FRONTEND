c91c16c5b5859d4a71a95aa0f5620c89886870ed
Muhammad Moiz <muhammadmoiztanveer@gmail.com>
2026-01-09 14:35:29 +0500

    force redeploy

--- start-server.sh
+++ start-server.sh
@@ -4,7 +4,7 @@
 # Set defaults if not provided by Railway
 export NODE_ENV=${NODE_ENV:-production}
 export HOSTNAME=${HOSTNAME:-0.0.0.0}
-export PORT=${PORT:-3000}
+export PORT=${PORT:-8080}
 
 # Start server from project root (server.js will change to its own directory)
 exec node .next/standalone/server.js
